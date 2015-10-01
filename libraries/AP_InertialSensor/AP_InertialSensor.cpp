882c2c0b07f4f158f385b719527bc18e60411820
skyscraper <kwikius@yahoo.com>
2015-10-01 07:52:18 +0100

    add quan placeholder to ins

--- libraries/AP_InertialSensor/AP_InertialSensor.cpp
+++ libraries/AP_InertialSensor/AP_InertialSensor.cpp
@@ -516,6 +516,8 @@ AP_InertialSensor::_detect_backends(void)
     //_add_backend(AP_InertialSensor_L3GD20::detect);
     //_add_backend(AP_InertialSensor_LSM303D::detect);
     _add_backend(AP_InertialSensor_MPU6000::detect_spi(*this));
+#elif HAL_INS_DEFAULT == HAL_INS_QUAN
+#pragma message "TODO QUAN INS"
 #else
     #error Unrecognised HAL_INS_TYPE setting
 #endif
