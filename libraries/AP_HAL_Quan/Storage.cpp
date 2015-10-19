379e4b1b3775471d582ebfa60949bce6c0872089
skyscraper <kwikius@yahoo.com>
2015-10-19 20:39:38 +0100

    basic work on Storage lib for quan

--- libraries/AP_HAL_Quan/Storage.cpp
+++ libraries/AP_HAL_Quan/Storage.cpp
@@ -1,6 +1,16 @@
 
-#include <string.h>
+
+
 #include "Storage.h"
+
+extern const AP_HAL::HAL& hal;
+
+namespace Quan{
+
+    bool eeprom_read(void * buffer,uint16_t eeprom_address,size_t n);
+    bool eeprom_write(uint16_t eeprom_address, void const * buffer,size_t n);
+
+}
 
 using namespace Quan;
 
@@ -10,10 +20,17 @@ QuanStorage::QuanStorage()
 void QuanStorage::init(void*)
 {}
 
-void QuanStorage::read_block(void* dst, uint16_t src, size_t n) {
-    memset(dst, 0, n);
+void QuanStorage::read_block(void* dst, uint16_t eeprom_address, size_t n) 
+{
+   if ( !Quan::eeprom_read(dst,eeprom_address,n) ){
+      hal.scheduler->panic("eeprom read failed");
+   }
 }
 
-void QuanStorage::write_block(uint16_t loc, const void* src, size_t n)
-{}
+void QuanStorage::write_block(uint16_t eeprom_address, const void* src, size_t n)
+{
+   if ( !Quan::eeprom_write(eeprom_address,src,n) ){
+      hal.scheduler->panic("eeprom write failed");
+   }  
+}
 
