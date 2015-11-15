48c4de3f108b917b789ef99f4fde47fa222a94e2
skyscraper <kwikius@yahoo.com>
2015-11-15 11:32:03 +0000

    replace assert by some led sequence for quan

--- libraries/AP_HAL/HAL.cpp
+++ libraries/AP_HAL/HAL.cpp
@@ -1,4 +1,8 @@
+
+#include <AP_HAL/AP_HAL.h>
+#if CONFIG_HAL_BOARD != HAL_BOARD_QUAN
 #include <assert.h>
+#endif
 
 #include "HAL.h"
 
@@ -8,8 +12,12 @@ HAL::FunCallbacks::FunCallbacks(void (*setup_fun)(void), void (*loop_fun)(void))
     : _setup(setup_fun)
     , _loop(loop_fun)
 {
+#if CONFIG_HAL_BOARD != HAL_BOARD_QUAN
     assert(setup_fun);
     assert(loop_fun);
+#else
+  // flash error led if fail
+#endif
 }
 
 }
