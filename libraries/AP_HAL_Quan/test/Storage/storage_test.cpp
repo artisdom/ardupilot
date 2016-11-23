04eb753f54eab54fcae0c0a399083835eb87e6dd
Andy Little <kwikius@yahoo.com>
2016-11-23 11:12:06 +0000

    modify the test for a larger input string. Requires to test that the eeprom write buffer has been flushed, else the read may return what was on the eeprom before the write buffer has been flushed to the eeprom

--- libraries/AP_HAL_Quan/test/Storage/storage_test.cpp
+++ libraries/AP_HAL_Quan/test/Storage/storage_test.cpp
@@ -4,12 +4,14 @@
 #include <AP_Progmem/AP_Progmem.h>
 #include <AP_Param/AP_Param.h>
 #include <StorageManager/StorageManager.h>
+#include <AP_HAL_Quan/Storage.h>
 #include <AP_Math/AP_Math.h>
 #include <AP_HAL_Quan/AP_HAL_Quan.h>
 #include <AP_HAL_Quan/AP_HAL_Quan_Test_Main.h>
 #include <AP_HAL/utility/functor.h>
 
 #include <quantracker/osd/osd.hpp>
+#include <quan/min.hpp>
 #include <task.h>
 #include <cstring>
 #include <stm32f4xx.h>
@@ -29,7 +31,27 @@ namespace {
    constexpr uint8_t pin_off = 0U;
    constexpr uint8_t pin_on = 1U;
 
-   constexpr uint32_t ee_addr = 6;
+   constexpr uint32_t ee_addr = 1253;
+
+   char constexpr text [] = "Simple Mixer \n"
+"# anything global must be a constant ...\n"
+"# type is deduced from the initialiser expression\n"
+"# There are 3 types integer, float bool\n"
+"# floats are differentiated by including a decimal point and fractional part\n"
+"# bools are either true or false\n"
+"# There is NO conversion between the different types\n"
+"#  x = 1 ; // x is an integer\n"
+"#  y = 1.0 ; // y is a float\n"
+"#  z1 = x + x ; // OK z1 is an integer\n"
+"#  z2 = x + y ; // Error : x and y are different types\n"
+"#  b = true;   // b is a bool\n"
+"#  c = b && x ;   // Error :  b and x are different types\n"
+"#  d = b && !b ; // ok ( c is false)\n"
+"#  w = x != 0  ; // w is bool \n"
+"\n"
+"pitch_gain = 500.0; # pitch gain surprisingly ( Just here to test comments)\n";
+
+char buffer[100U];
 
    struct test_task_t{
 
@@ -37,26 +59,36 @@ namespace {
 
       void fun()
       { 
-          char const text [] = "Hello There";
           uint32_t len = strlen(text) +1;
-       //   hal.gpio->toggle(red_led_pin);
           if ( m_count == false){
              hal.storage->write_block(ee_addr,text,len);
              m_count = true;
           }else{
              hal.console->printf("-------------------------\n");
-             char buffer[100] = {'\0'};
-             hal.storage->read_block(buffer,ee_addr,len);
-             if ( buffer[len-1] == '\0'){
-               hal.console->write("got something\n");
-               for ( uint32_t i= 0; i < len; ++i){
+
+ /*
+   The data is buffered in a queue and takes a while to be written to eeprom
+   so, before reading it, check that it has been flushed
+*/
+             Quan::wait_for_eeprom_write_queue_flushed();
+             
+/*
+      read the data to a small buffer a bit at a time
+*/
+             uint32_t len1 = len;
+             uint32_t ee_addr1 = ee_addr;
+             while (len1 > 0U){
+                uint32_t bytes_to_read = quan::min(100U,len1);
+                hal.storage->read_block(buffer,ee_addr1 ,bytes_to_read);
+                // we also need to beware of overrunning the uart buffer
+                while (hal.console->tx_pending() ){asm volatile ("nop":::);}
+                for ( uint32_t i= 0; i < bytes_to_read; ++i){
                   hal.console->printf("%c",(((char const*)buffer)[i]));
-               }
-               hal.console->write("\n");
-             }else{
-               hal.console->write("read failed\n");
+                }
+                ee_addr1 += bytes_to_read;
+                len1 -= bytes_to_read;
              }
-             hal.console->printf("-------------------------\n");
+             hal.console->printf("\n\n-------------------------\n");
              m_count = false;
           }
       };
