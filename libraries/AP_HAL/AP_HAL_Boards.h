dff67d1a04d194e4887a0d630abf1c4793f73d02
Andy Little <kwikius@yahoo.com>
2018-03-31 10:47:14 +0100

    move board defs to separate source

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -99,45 +99,9 @@
  */
 
 #if CONFIG_HAL_BOARD == HAL_BOARD_SITL
-#define HAL_BOARD_NAME "SITL"
-#define HAL_CPU_CLASS HAL_CPU_CLASS_1000
-#define HAL_OS_POSIX_IO 1
-#define HAL_OS_SOCKETS 1
-#define HAL_STORAGE_SIZE            16384
-#define HAL_STORAGE_SIZE_AVAILABLE  HAL_STORAGE_SIZE
-#define HAL_BOARD_LOG_DIRECTORY "logs"
-#define HAL_BOARD_TERRAIN_DIRECTORY "terrain"
-#define HAL_PARAM_DEFAULTS_PATH "etc/defaults.parm"
-#define HAL_INS_DEFAULT HAL_INS_HIL
-#define HAL_BARO_DEFAULT HAL_BARO_HIL
-#define HAL_COMPASS_DEFAULT HAL_COMPASS_HIL
-namespace HALSITL {
-   struct tag_board;
-}
-
-namespace AP_HAL{
-   typedef HALSITL::tag_board board;
-}
-
+  #include <AP_HAL/board/Sitl.h>
 #elif CONFIG_HAL_BOARD == HAL_BOARD_QUAN
-#define AP_HAL_BOARD_DRIVER AP_HAL_Quan
-#define HAL_BOARD_NAME "QUAN"
-// NB This causes issues since if its class 150 then code always assumes PX4
-// 
-#define HAL_CPU_CLASS HAL_CPU_CLASS_150
-#define HAL_STORAGE_SIZE            4096
-#define HAL_STORAGE_SIZE_AVAILABLE  HAL_STORAGE_SIZE
-#define HAL_INS_DEFAULT HAL_INS_QUAN
-#define HAL_BARO_DEFAULT HAL_BARO_QUAN
-#define HAL_COMPASS_DEFAULT HAL_COMPASS_QUAN
-#define CONFIG_HAL_BOARD_SUBTYPE HAL_BOARD_SUBTYPE_NONE
-namespace Quan {
-   struct tag_board;
-}
-
-namespace AP_HAL{
-   typedef Quan::tag_board board;
-}
+  #include <AP_HAL/board/Quan.h>
 #else
 #error "Unknown CONFIG_HAL_BOARD type"
 #endif
